cb05536967c27f466e7efb63c25b5b50f285bfcf
Vooltexx <152155025+Vooltexx@users.noreply.github.com>
2024-02-06 17:51:15 +0300

    Add files via upload

---
+++ ConsoleApp00000000/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp00000000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Вывести в строку десять заглавных букв "а" через один пробел:
+
+
+            // for (int x = 0; x <= 10; x++)
+            // {
+            // Console.Write(" a ");
+
+            // }
+
+            //2.Вывести в обратном порядке числа от 50 до 40.
+
+            //for (int x = 50; x >= 40; x--)
+            //{
+            // Console.Write(x + " ");
+            //}
+
+
+
+            //3.Вывести в строку через пробел следующие натуральные числа
+            //for (int x = 6; x <= 36; x += 6)
+            //{
+            // Console.Write(x + " ");
+            //}
+
+            //4.Вывести двузначные числа, кратные 11, подсчитать их количество и сумму
+            //int count = 0;
+            //int sum = 0;
+            //for (int x = 10; x <= 99; x++)
+            //{
+            //    if (x % 11 == 0)
+            //    {
+            //        Console.WriteLine(x);
+            //        count++;
+            //        sum += x;
+            //    }
+            //}
+            //Console.WriteLine($"Количество {count},Сумма {sum}");
+            Console.ReadKey();
+        }
+    }
+}
